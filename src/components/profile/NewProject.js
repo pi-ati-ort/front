e2f40263b982ec14c2285350ecd3ad67c48741fb
Nicolas <nicolas@nicolasf.uy>
2024-02-19 15:39:25 -0300

    mejoras

--- src/components/profile/NewProject.js
+++ src/components/profile/NewProject.js
@@ -94,7 +94,6 @@ const NewProject = () => {
         });
       })
       .catch((error) => {
-        console.log(error);
         toast.error("Ocurrió un error", {
           position: "bottom-right",
           autoClose: 2000,
@@ -152,7 +151,6 @@ const NewProject = () => {
       model.projectId != null
     ) {
       uploadModelToDatabase(createdProject.id, model);
-      console.log("Modelo", model);
     }
     /* uploadModelToProject(createdProject.id, formData).then((res) => {
       console.log(res);
@@ -484,10 +482,12 @@ const NewProject = () => {
         <>
           <div className="bg-white p-6 h-80 rounded-2xl shadow-lg flex flex-col border border-idem mt-4 mb-2">
             <h2 className="text-2xl font-semibold">
-              Cargar Modelo <span className="text-lg text-idem font-bold">*</span>
+              Cargar Modelo{" "}
+              <span className="text-lg text-idem font-bold">*</span>
             </h2>
             <p className=" text-gray-500 font-medium text-sm mb-2">
-              Debe cargar un modelo IFC compatible con este proyecto, seleccione uno{" "}
+              Debe cargar un modelo IFC compatible con este proyecto, seleccione
+              uno{" "}
               <a
                 href="https://github.com/pi-ati-ort/ifc-models"
                 target="_blank"
